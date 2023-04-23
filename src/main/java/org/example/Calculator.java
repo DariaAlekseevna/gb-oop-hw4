2effb44337df00b71a8ca7fbf55413c37226ffcd
unknown <dasha.gugnina98@gmail.com>
2023-04-23 06:34:18 +0300

    update the  binary method

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -4,32 +4,33 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 //@NoArgsConstructor
 public class Calculator {
+
     /**
      * @param list список из цифр
      * @return сложение всех элементов в списке
      */
-    public Double sum(List<? extends Number> list){
-        Double sum =0.0;
-        for (Number elem: list) {
-            sum+= elem.doubleValue();
+    public Double sum(List<? extends Number> list) {
+        Double sum = 0.0;
+        for (Number elem : list) {
+            sum += elem.doubleValue();
         }
         return sum;
-    };
-
+    }
 
     /**
      * @param list список цифр
      * @return перемножение всех элементов в списке
      */
-    public Double multiply(List<? extends Number> list){
+    public Double multiply(List<? extends Number> list) {
         Double multi = 1.0;
-        for (Number elem: list) {
+        for (Number elem : list) {
             multi *= elem.doubleValue();
         }
         return multi;
@@ -39,9 +40,9 @@ public class Calculator {
      * @param list список из цифр
      * @return деление первого элемента поочередно на все элементы в списке
      */
-    public Double division(List<? extends Number> list){
+    public Double division(List<? extends Number> list) {
         Double div = list.get(0).doubleValue();
-        for (Number elem: list) {
+        for (Number elem : list) {
             if (elem.doubleValue() != 0.0) {
                 div /= elem.doubleValue();
             } else {
@@ -52,8 +53,58 @@ public class Calculator {
         return div;
     }
 
-    public Double conversionToBinary(List<? extends Number> list){
-        return null;
+    /**
+     * @param list список с любыми значениями
+     * @return список преобразованный в формат Integer или сообщение об ошибке и null
+     */
+    public List<Integer> listConversionToTheRequiredFormat(List<?> list) {
+        List<Integer> formatList = new ArrayList<>();
+        List<String> transitList = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) instanceof String) {
+                if (((String) list.get(i)).length() > 2 &&
+                        ((String) list.get(i)).substring(((String) list.get(i)).length() - 2).equals(".0")) {
+                    transitList.add(((String) list.get(i)).replace(".0", ""));
+                    formatList.add(Integer.parseInt(transitList.get(i)));
+                } else {
+                    try {
+                        formatList.add(Integer.parseInt((String) list.get(i)));
+                    } catch (NumberFormatException e) {
+                        System.out.println(e);
+                        System.out.println("программа не позволяет преобразовать строку данного типа в числовой вид," +
+                                " подходящий для дальнейшего преобразования в бинарный вид!!");
+                        return null;
+                    }
+                }
+            } else if (list.get(i) instanceof Number) {
+                try {
+                    formatList.add(((Number) list.get(i)).intValue());
+                } catch (NumberFormatException e) {
+                    System.out.println(e);
+                    System.out.println("программа не позволяет получить тип Integer," +
+                            " подходящий для дальнейшего преобразования в бинарный вид!!");
+                }
+            }
+
+        }
+        return formatList;
+    }
+
+    /**
+     * @param list список c любыми значениями
+     * @return список, значения которого преобразованы в бинарный формат или сообщение об ошибке и null
+     */
+    public List<String> conversionToBinary(List<?> list) {
+        List<String> binList = new ArrayList<>();
+        if (listConversionToTheRequiredFormat(list) != null) {
+            for (Integer elem : listConversionToTheRequiredFormat(list)) {
+                binList.add(Integer.toBinaryString(elem.intValue()));
+            }
+            return binList;
+        } else {
+            System.out.println("программа не содержит функционал для конвертации введенных данных.");
+            return null;
+        }
     }
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,6 +13,10 @@ public class Main {
         List<Integer> listI2 = new ArrayList<>();
         List<Double> listD3 = new ArrayList<>();
         List<Integer> listI3 = new ArrayList<>();
+        List<String> listS = new ArrayList<>();
+        List<String> listS2 = new ArrayList<>();
+        List<String> listS3 = new ArrayList<>();
+        List<String> listS4 = new ArrayList<>();
 
         for (int i = 0; i < 6; i++) {
             listD.add((double) i);
@@ -22,21 +26,40 @@ public class Main {
             listD2.add((double) i);
             listI2.add(i);
         }
-
         for (int i = 6; i > 1; i -= 2) {
             listD3.add((double) i);
             listI3.add(i);
         }
+        for (int i = 0; i < 6; i++) {
+            listS.add(i + ".0");
+        }
+        for (int i = 0; i < 6; i++) {
+            listS2.add(i + ".1");
+        }
+        for (int i = 0; i < 6; i++) {
+            listS3.add("" + i);
+        }
+        for (int i = 0; i < 6; i++) {
+            listS4.add("meow");
+        }
+
         System.out.println("Sum of elements in the (Double) list: " + calc.sum(listD));
         System.out.println("Sum of elements in the (Integer) list: " + calc.sum(listI));
 
-        System.out.println("Product of elements in the second (Integer) list: " + calc.multiply(listI2));
-        System.out.println("Product of elements in the second (Double) list: " + calc.multiply(listD2));
+        System.out.println("Product of elements in the (Integer) list2: " + calc.multiply(listI2));
+        System.out.println("Product of elements in the Double) list2: " + calc.multiply(listD2));
         System.out.println("Product of elements in the (Double) list: " + calc.multiply(listD));
 
-        System.out.println("Division of elements in third (Double) list : " + calc.division(listD3));
-        System.out.println("Division of elements in third (Integer) list: " + calc.division(listI3));
+        System.out.println("Division of elements in (Double) list3: " + calc.division(listD3));
+        System.out.println("Division of elements in (Integer) list3: " + calc.division(listI3));
         System.out.println("Division of elements in the (Integer) list: " + calc.division(listI));
+
+        System.out.println("(Integer)List conversion to binary: " + calc.conversionToBinary(listI));
+        System.out.println("(Double)List conversion to binary: " + calc.conversionToBinary(listD));
+        System.out.println("(String)List conversion to binary: " + calc.conversionToBinary(listS));
+        System.out.println("(String)List2 conversion to binary: " + calc.conversionToBinary(listS2));
+        System.out.println("(String)List3 conversion to binary: " + calc.conversionToBinary(listS3));
+        System.out.println("(String)List4 conversion to binary: " + calc.conversionToBinary(listS4));
 
     }
 }
